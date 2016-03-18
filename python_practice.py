de9ce9cb3344e97896e4f65e99b4669b990091bc
GA Sauer <gsr732@gmail.com>
2016-03-18 17:34:07 -0400

    Added files via upload
    Python program that will prompt user for their name
    ask age and calculate in how many years 
    they will be 100

---
+++ python_practice.py
@@ -0,0 +1,25 @@
+'''
+This program will promput user for their name
+ask age and calculate in how many years 
+they will be 100
+'''
+
+from datetime import date
+import os
+
+#Prompt user for their name
+name = input("What is your name?  ")
+
+#Prompt user for their age
+age = input("what is your age? ")
+
+#Get today's date
+currentDate = date.today()
+
+#from currentDate parse year
+currentYear = currentDate.year
+
+when100 = ((100-int(age)))
+
+print(name + ' you\'ll be 100 in ' + str(when100) + ' years!')
+
