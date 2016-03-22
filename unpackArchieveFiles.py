d4c84cf095d79ce72b1c5585e70d93fea4213acb
GA Sauer <gsr732@gmail.com>
2016-03-22 11:21:35 -0400

    Added files via upload
    Script that will unpack gz file.

---
+++ unpackArchieveFiles.py
@@ -0,0 +1,18 @@
+"""
+This script will extract archive files 
+In this case a .gz file
+
+auther:  GA Sauer
+date:    March 22, 2016
+"""
+
+import os, patoolib
+
+#Set working directory
+os.chdir("/Users/GAMac/pythonPractice/gz_files")
+
+#Unpack archiver files
+patoolib.extract_archive("amie2.20160321.gz", outdir="/Users/GAMac/pythonPractice/gz_unpack")
+
+
+
